0a4c7b649c1d1bbc87cb73f0c5e10fae6f37e4d0
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2021-09-30 09:48:48 -0400

    prints a message

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
  * main - print a message
  * return: 0
  */
-int main (void)
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
