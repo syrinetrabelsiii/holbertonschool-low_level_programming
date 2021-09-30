0e55a380763e3ff580fc3f81ab98963d29dfabe8
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2021-09-30 09:36:48 -0400

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-/* function main begins program execution */
+/**
+ * main - prints a message
+ * Return: 0
+ */
 int main( void ) 
 {
-	puts ("\"Programming is like building a multilingual puzzle" ); 
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
