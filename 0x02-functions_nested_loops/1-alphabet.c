4bcb995e9bae9532d0a94aff8385a86c8712d33e
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2021-10-05 05:23:43 -0700

    prints alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,25 +3,12 @@
  * main - print alphabets
  * Return: 0
  */
-void    ft_print_alphabet(void)
-
+void print_alphabet(void)
 {
-    char    letter;
+	int n;
 
-    letter = 'a';
-    while (letter <= 'z')
-    {
-        putchar(letter);
-        letter++;
-    }
+	for (n = 'a'; n <='z'; n++)
+		putchar(n);
+	putchar('\n');
 }
 
-int main(void)
-{
-    ft_print_alphabet();
-    return 0;
-}i
-		printf("%c ", c);
-	printf("\n");
-	return (0);
-}
