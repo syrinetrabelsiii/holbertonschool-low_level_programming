f22b4d4b472d938009529ea20c7ca0a6731c8454
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2021-10-05 02:08:48 -0700

    prints alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - print alphabets
+ * Return: 0
+ */
+void    ft_print_alphabet(void)
+
+{
+    char    letter;
+
+    letter = 'a';
+    while (letter <= 'z')
+    {
+        putchar(letter);
+        letter++;
+    }
+}
+
+int main(void)
+{
+    ft_print_alphabet();
+    return 0;
+}i
+		printf("%c ", c);
+	printf("\n");
+	return (0);
+}
