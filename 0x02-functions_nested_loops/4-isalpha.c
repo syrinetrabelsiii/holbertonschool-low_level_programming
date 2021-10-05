13b5fb81f24cac98bf449bb475387a17e0064c5e
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2021-10-05 06:45:55 -0700

    checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: int
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'Z')
+	{
+	return (1);
+	}
+	return (0);
