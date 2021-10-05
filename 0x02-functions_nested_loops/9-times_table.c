7f99eb25c82e82d85dece1ca4ac85b8c3a8521d3
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2021-10-05 15:57:05 -0700

    time of table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * time_table - time of table
+ * Return: 0 
+ */
+void times_table(void)
+{
+	int i, j, h, e, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			k = i * j;
+			h = k / 10;
+			e = k % 10;
+			if (j == 0)
+			{
+				_putchar(' ');
+				_putchar('0' + e);
+			}
+			else
+			{
+				_putchar('0' + h);
+				_putchar('0' + e);
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
