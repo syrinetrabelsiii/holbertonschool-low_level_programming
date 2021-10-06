3d054c391884e0c7b022fb28ec7971b71c805bc2
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2021-10-06 02:12:49 -0700

    prints all natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,11 +8,11 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (n = n; n <= 97; n++);
-		printf("%d ,", n);
+		for (n = n; n <= 97; n++)
+			printf("%d ,", n);
 	}
 	else
-		for (n = n; n > 98; n--);
-	printf("%d ,", n);
+		for (n = n; n > 98; n--)
+			printf("%d ,", n);
 	printf("98\n");
 }
