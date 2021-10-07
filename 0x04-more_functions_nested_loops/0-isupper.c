7bbf6d051a05663f9e6544e3a515739394e1a080
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2021-10-07 02:16:03 -0700

    checks for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ * @c: int
+ * @i: char
+ * Return: 0
+ */
+int _isupper(int c)
+
+{
+	char i;
+
+	for (i = 'A'; i <= 'Z'; i++)
+		if (c >= 'A' && c <= 'Z')
+			return (1);
+	return (0);
+}
