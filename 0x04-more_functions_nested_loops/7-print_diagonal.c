c7379e900f1edb9a9d71fd1322c5d9dd3b5eaf3b
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2021-10-07 06:46:26 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,13 +4,22 @@
  * @n: int
  */
 void print_diagonal(int n)
-{
-	int i;
 
+{
+	int i, j;
+
+	if (n > 0)
 	for (i = 0; i < n; i++)
-	{
-		if (n > 0)
-			_putchar(' ');
-	}
+{
+	for (j = 0; j < i; j++)
+{
+	_putchar(' ');
+}
+	_putchar('\\');
 	_putchar('\n');
+}
+	else
+{
+	_putchar('\n');
+}
 }
