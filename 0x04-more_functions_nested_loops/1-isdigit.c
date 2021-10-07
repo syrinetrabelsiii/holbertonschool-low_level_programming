d46fa3faf07459a4510338d06de1972191d7bb54
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2021-10-07 04:36:21 -0700

    checks digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,14 +2,12 @@
 /**
  * _isdigit - checks for a digit
  * @c : int
- * @i : int
  * Return: 0
  */
 int _isdigit(int c)
 {
 	int i;
 
-	for (i = '0'; i <= '9'; i++)
 		if (c >= '0' && c <= '9')
 			return (0);
 	return (0);
