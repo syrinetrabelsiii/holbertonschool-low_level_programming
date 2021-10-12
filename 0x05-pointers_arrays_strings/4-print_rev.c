15fa6a840d448395dd84a04b1c346e8a91d25cd9
syrinetrabelsiii <syrinetrabelsi90@gmail.com>
2021-10-12 02:40:42 -0700

    prints a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - prits a string in reverse
+ * @s : char
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	while (i > 0)
+	{
+		_putchar(s[i - 1]);
+		i--;
+	}
+	_putchar('\n');
+}
+
