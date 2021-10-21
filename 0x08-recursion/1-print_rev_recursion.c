a733c61c2dcca8bbe53b8aecde66b7db493fb164
syrinetrabelsiii <syrinetrabelsi90@gmail.com>
2021-10-21 04:47:35 -0700

    reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _print_rev_recursion -prints string in reverse 
- * @s :char
- * Return: 0
+ * _print_rev_recursion - prints string in reverse 
+ * @s: char
  */
 void _print_rev_recursion(char *s)
 {
 	if (*s)
-		_print_rev_recursion(s + 1);
+	{
+	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }
-		
+}	
