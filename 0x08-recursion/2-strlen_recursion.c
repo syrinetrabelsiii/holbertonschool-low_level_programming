ceb60f641dcaa8c671518f701861fc11657692e1
syrinetrabelsiii <syrinetrabelsi90@gmail.com>
2021-10-21 05:07:33 -0700

    returns the lenght of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length
+ * @s: char
+ */
+int _strlen_recursion(char *s)
+{
+if (*s != '\0')
+return (1 + _strlen_recursion(s + 1));
+return (0);
+}
