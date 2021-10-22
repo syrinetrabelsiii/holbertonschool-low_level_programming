922087b2f9d7b22284315ba483c781a7581a7351
syrinetrabelsiii <syrinetrabelsi90@gmail.com>
2021-10-22 05:30:27 -0700

    prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,9 +3,10 @@
  * main - prints its name
  * @argc: int
  * @argv: int
+ * Return: int
  */
-int main (__attribute__((unused))int argc, char *argv[])
+int main(__attribute__((unused))int argc, char *argv[])
 {
-	printf ("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
