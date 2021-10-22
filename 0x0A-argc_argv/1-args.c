07947a24db9bccd775a40d982276cbec6180987d
syrinetrabelsiii <syrinetrabelsi90@gmail.com>
2021-10-22 03:54:28 -0700

    prints numbers of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - prints the number of arg
+ * @argc: int
+ * @argv: int
+ * Return: int
+ */
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
+
