b03c74041b43aefdd398f7e5e29dbdba7968eca9
syrinetrabelsiii <syrinetrabelsi90@gmail.com>
2021-10-22 03:24:37 -0700

    prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - prints its name
+ * @argc: int
+ * @argv: int
+ */
+int main (__attribute__((unused))int argc, char *argv[])
+{
+	printf ("%s\n", argv[0]);
+	return (0);
+}
