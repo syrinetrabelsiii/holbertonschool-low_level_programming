06d912ad66b0334736372ddd7cf0e3a621a649ad
syrinetrabelsiii <syrinetrabelsi90@gmail.com>
2021-10-24 14:46:48 -0700

    header

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,12 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-#include <ctype.h>
-#include <locale.h>
-#include <unistd.h>
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -27,5 +20,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int main(void);
+
 #endif
