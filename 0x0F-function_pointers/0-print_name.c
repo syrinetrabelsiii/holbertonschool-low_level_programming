aee0d042978cbccc6c32ba890b49113dc6aab5c6
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2021-11-04 05:13:16 -0400

    prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,13 @@
 #include "function_pointers.h"
 /**
  * void print_name - prints name
+ * @name: char
+ * @f: char
  * Return: string
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		f(name);
+if (name != NULL && f != NULL)
+	f(name);
 }
