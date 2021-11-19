4491b0c5dbd0506cd8a0a9a872c9756e1e6cfe47
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2021-11-19 13:45:52 +0100

    converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,10 +10,10 @@ unsigned int k = 1;
 unsigned int i = 0;
 int c;
 unsigned int len;
-len = strlen(b);
 
 if (b == NULL)
 return (0);
+len = strlen(b);
 for (c = len - 1; c >= 0; c--)
 {
 if (b[c] != '0' && b[c] != '1')
