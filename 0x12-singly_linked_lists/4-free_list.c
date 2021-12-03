ec09d39f41bc3f55361f100d1d93d177c71f0e99
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2021-12-03 23:22:25 +0100

    frees a list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list.
+ *@head: pointer.
+ *Return: nothing.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *T;
+
+	while (head != NULL)
+	{
+		T = head->next;
+		free(head->str);
+		free(head);
+		head = T;
+	}
+}
