c827d108134cfd5c1ad267d1ff8e436c79ff6605
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2022-08-31 23:28:55 +0100

    binary search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+/**
+ * binary_search - implement Binary Search
+ * @array: pointer
+ * @size_t: size of array
+ * @value: value to search for
+ * Return: 
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+    
+     if (r >= l) {
+        int mid = l + (r - l) / 2;
+}
